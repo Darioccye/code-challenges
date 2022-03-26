1e02699645a6f112dd65bb3dc51f3d51f3fe0221
Dario Chen <dariochenchenye@gmail.com>
2022-03-26 03:24:47 -0300

    Adicionando exercícios

--- Strings/1632.py
+++ Strings/1632.py
@@ -4,7 +4,7 @@ for n in range(numero_de_casos):
     combinacoes = 1
     senha = input()
     for letra in senha:
-        if letra == 'a' or letra == 'A' or letra == 'e' or letra == 'E' or letra == 'O' or letra == 'o' or letra == 'i' or letra == 'I' or letra == 's' or letra == 'S':
+        if letra in 'aeios' or letra in 'AEIOS':
             combinacoes *= 3
         else:
             combinacoes *= 2
---
+++ Strings/3358.py
@@ -0,0 +1,15 @@
+quantidade_de_nomes = int(input())
+
+for n in range(quantidade_de_nomes):
+    nome = input()
+    nome.split()
+    dificuldade = 0
+    for n in range(len(nome)-2):
+        if nome[n] not in 'aeiou' and nome[n+1] not in 'aeiou' and nome[n+2] not in 'aeiou':
+            dificuldade += 1
+        else:
+            dificuldade += 0
+    if dificuldade > 0:
+        print("%s nao eh facil" % ''.join(nome))
+    if dificuldade == 0:
+        print("%s eh facil" % ''.join(nome))
